3d8b3a42ad3fce85457ef2b4b6b88be227f60cc5
SujibShrestha <sujeeb98@gmail.com>
2025-09-12 18:31:27 +0545

    checking api

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser'
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,//ONLY ALLOW FROM THIS
     credentials: true,
 }))
 
@@ -15,5 +15,13 @@ app.use(express.urlencoded({extended:true,
 }))
 app.use(express.static('public'))
 app.use(cookieParser())
+
+
+//routes
+import userRouter from './routes/user.routes.js'
+
+
+//route declaration (IN URL)
+app.use('/api/v1/users',userRouter)//MIDDLEWARE 
 
 export default app
